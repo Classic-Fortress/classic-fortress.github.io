9a60c781c6edaede2337b3d9d6b3b302494490cd
Jocke <lindstrm@gmail.com>
2015-07-21 12:43:19 +0200

    Fix naming errors

--- resources/views/Forum/_parts/controls.blade.php
+++
@@ -1,8 +0,0 @@
-@if(checkAdmin())
-    <div class="forum-controls">
-        <a href="{{ action('Forum\ForumController@create') }}"><i class="fa fa-plus" title="Create new forum"></i></a>
-        @if(isset($topics))
-        <a href="{{ action('Forum\ForumController@edit', ['forum' => $forum->slug]) }}"><i class="fa fa-wrench" title="Edit forum"></i></a>
-        @endif
-    </div>
-@endif
