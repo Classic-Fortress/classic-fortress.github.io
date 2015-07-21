8bf007c115161163583517de0052e3fe1fce4963
Jocke <lindstrm@gmail.com>
2015-07-21 18:12:24 +0200

    Add edit button to wiki pages

--- resources/views/wiki.blade.php
+++ resources/views/wiki.blade.php
@@ -6,7 +6,12 @@
     <div class="wiki">
         <div class="clearfix">
             <h1 class="pull-left">Wiki <small>{{ str_replace('-',' ', $page) }}</small></h1>
-            <div class="toggle-wiki-sidebar pull-right" title="Wiki Menu"><i class="fa fa-bars"></i></div>
+            <div class="pull-right">
+                @if(checkAdmin() or checkModerator())
+                    <a href="https://github.com/Classic-Fortress/server-qwprogs/wiki/{{ $page }}/_edit" title="Edit this page"><i class="fa fa-pencil-square"></i></a>
+                @endif
+                <a class="toggle-wiki-sidebar" title="Wiki Menu"><i class="fa fa-bars"></i></a>
+            </div>
         </div>
         <div class="wiki-sidebar hidden" markdown>
             {!! str_replace('**[Go to front page](Home)**','',file_get_contents('https://raw.githubusercontent.com/wiki/Classic-Fortress/server-qwprogs/_Sidebar.md')) !!}
