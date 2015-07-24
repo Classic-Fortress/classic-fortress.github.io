bf89594355997bda0f3d3461b37dcdc6c8ca6da6
jocke <lindstrm@gmail.com>
2015-07-24 16:50:25 +0200

    Add link to comments #41

--- resources/views/_parts/news.blade.php
+++ resources/views/_parts/news.blade.php
@@ -4,7 +4,8 @@
             <a href="{{ route('forum.topic',['forum'=>'news', 'id' => $entry->id, 'topic' => $entry->slug]) }}">
                 {{ $entry->title }}
             </a>
-            <small>{{$entry->created_at->format('j M y, H:i')}} by <a href="{{ route('user', ['user' => $entry->author->slug]) }}">{{ $entry->author->username }}</a> | ({{ $entry->messages->count()-1 }} {{ str_plural('comment', ($entry->messages->count()-1)) }})</small>
+            <small>{{$entry->created_at->format('j M y, H:i')}} by <a href="{{ route('user', ['user' => $entry->author->slug]) }}">{{ $entry->author->username }}</a> |
+                (<a href="{{ route('forum.topic',['forum'=>'news', 'id' => $entry->id, 'topic' => $entry->slug]) }}">{{ $entry->messages->count()-1 }} {{ str_plural('comment', ($entry->messages->count()-1)) }}</a>)</small>
         </h3>
         <p markdown>{!! $entry->messages[0]->parse()->body !!}</p>
     </article>
