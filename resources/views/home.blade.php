59abdd04466732e0b60e85f6a3b4e4ddaf2c1c94
jocke <lindstrm@gmail.com>
2015-09-15 12:25:14 +0200

    Fix intro video

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
 
     <div markdown>{!! file_get_contents("https://raw.githubusercontent.com/wiki/Classic-Fortress/server-qwprogs/Page-Home.md") !!}</div>
 
+    <iframe width="674" height="379" src="//www.youtube.com/embed/gw247s0oMK0" frameborder="0" allowfullscreen></iframe>
+
     <hr/>
 
     <h2>Recent news</h2>
